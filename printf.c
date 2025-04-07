4b3765508a1f92184d687d2e0794b8583e9555c1
vishalpalDI <vishal.p@diverseinfotech.com>
2025-04-07 12:39:30 +0530

    simple output implemented

--- printf.c
+++ printf.c
@@ -1,8 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int len(char *text);
+void myprintf(char *text);
+
 int main()
 {
-    printf("%s", "Working!");
+    // printf("%d\n", len("Hello"));
+    myprintf("Hello");
     return 0;
+}
+
+void myprintf(char *text)
+{
+    write(STDOUT_FILENO, text, len(text) * sizeof(char));
+}
+
+int len(char *text)
+{
+    int i = 0;
+    while (*(text) != NULL)
+    {
+        text++;
+        i++;
+    }
+    return i;
 }
